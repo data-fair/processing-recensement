88460e0f61a0a7c9dae0b45618ddb97c896a6090
BatLeDev <baptiste.guerny@gmail.com>
2024-02-02 16:24:56 +0100

    feat: Update schema: set array of year

--- test/test.js
+++ test/test.js
@@ -8,36 +8,36 @@ describe('test', function () {
     this.timeout(1000000)
 
     // Creation d'un dataset
-    // const context = testUtils.context({
-    //   pluginConfig: {
-    //     apiAccessToken: config.apiAccessToken
-    //   },
-    //   processingConfig: {
-    //     datasetMode: 'create',
-    //     dataset: { title: 'Recensement Test' },
-    //     codeType: 'code_departement',
-    //     code: '973',
-    //     years: '2006',
-    //     clearFile: true
-    //   },
-    //   tmpDir: 'data'
-    // }, config, false)
-
-    // Mise à jour du dataset
     const context = testUtils.context({
       pluginConfig: {
         apiAccessToken: config.apiAccessToken
       },
       processingConfig: {
-        datasetMode: 'update',
-        dataset: { title: 'Recensement Test', id: 'xk6bmhg847-pf9hgcmtyk3ri' },
+        datasetMode: 'create',
+        dataset: { title: 'Recensement Test' },
         codeType: 'code_departement',
         code: '974',
-        years: '2006-2008',
-        clearFile: false
+        years: [2006],
+        clearFile: true
       },
       tmpDir: 'data'
     }, config, false)
+
+    // Mise à jour du dataset
+    // const context = testUtils.context({
+    //   pluginConfig: {
+    //     apiAccessToken: config.apiAccessToken
+    //   },
+    //   processingConfig: {
+    //     datasetMode: 'update',
+    //     dataset: { title: 'Recensement Test', id: 'xk6bmhg847-pf9hgcmtyk3ri' },
+    //     codeType: 'code_departement',
+    //     code: '974',
+    //     years: [2006],
+    //     clearFile: false
+    //   },
+    //   tmpDir: 'data'
+    // }, config, false)
 
     await rencensementProcessing.run(context)
   })
